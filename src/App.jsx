0a1c198eaa451797a7773f96def2705abd31ef9b
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-19 18:25:37 +0600

    RefForm created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+// import RefForm from './Componenets/RefForm/RefForm'
 // import SimpleForm from './Componenets/SimpleForm/SimpleForm'
 import StateFullForm from './Componenets/StateFullForm/StateFullForm'
 
@@ -12,6 +13,7 @@ function App() {
       <h1>Form Master</h1>
       {/* <SimpleForm></SimpleForm> */}
       <StateFullForm></StateFullForm>
+      {/* <RefForm></RefForm> */}
       
     </>
   )
