c658087c88d02e14c79c1881f0cf719856d2e69f
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-19 11:34:02 +0600

    project setup and create simple form created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import SimpleForm from './Componenets/SimpleForm/SimpleForm'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Form Master</h1>
+      <SimpleForm></SimpleForm>
+      
+    </>
+  )
+}
+
+export default App
